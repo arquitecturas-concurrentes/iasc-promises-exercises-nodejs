d20f445fa14a4f1207f6050747aab7ad45ac37a6
Ernesto Bossi <bossi.ernestog@gmail.com>
2017-08-29 17:36:06 -0300

    Add composition

---
+++ spec/6-composition-spec.js
@@ -0,0 +1,38 @@
+var assert = require("assert");
+var math = require("../src/6-composition.js");
+
+describe("promises", function() {
+
+    it("can implement functions successfully", function(done){
+        math
+            .succ(1)
+            .onResolved(function(value){
+                assert.equal(value, 2);
+                done();
+            })
+    })
+
+    it("can be mapped", function(done){
+        math
+            .succ(1)
+            .map(function(value) {
+                return (value * 2);
+            })
+            .onResolved(function(value){
+                assert.equal(value, 4);
+                done();
+            })
+    })
+
+    it("can be flatMapped", function(done){
+        math
+            .succ(1)
+            .flatMap(function(value) {
+                return math.succ(value);
+            })
+            .onResolved(function(value){
+                assert.equal(value, 3);
+                done();
+            })
+    })
+})
---
+++ src/6-composition.js
@@ -0,0 +1,43 @@
+function Promise(cont) {
+    this.cont = cont;
+}
+
+Promise.prototype = {
+
+    map: function(mapper) {
+        return this._derive(function(value, cont) {
+            cont(mapper(value));
+        });
+    },
+
+    flatMap: function(flatMapper) {
+        return this._derive(function(value, cont) {
+            var promise = flatMapper(value);
+            promise.onResolved(cont);
+        });
+    },
+
+    _derive: function(derived) {
+        var originalCont = this.cont;
+        return new Promise(function(cont){
+            originalCont(function(value) {
+                derived(value, cont);
+            });
+        });
+    },
+
+    onResolved: function(f) {
+        this.cont(f);
+    }
+}
+
+function succ(x) {
+    function succCPS(cont) {
+        cont(x + 1);
+    }
+    return new Promise(succCPS);
+}
+
+module.exports = {
+    succ: succ
+};
